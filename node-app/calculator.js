a06781e9e706171e66d2d229138db65428cbee09
Igor Semenkov <semenkov.box@gmail.com>
2020-07-13 08:17:06 +0300

    HW-21

---
+++ node-app/calculator.js
@@ -0,0 +1,17 @@
+module.exports = {
+    set: function (argument) {
+        return baseValue = argument;
+    },
+    add: function (argument) {
+        return baseValue += argument;
+    },
+    sub: function (argument) {
+        return baseValue -= argument;
+    },
+    mult: function (argument) {
+        return baseValue *= argument;
+    },
+    div: function (argument) {
+        return baseValue /= argument;
+    }
+}
